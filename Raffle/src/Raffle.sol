e05426688dc119ab6c5308b64553cc84c8356edf
raopremrao <raom61337@gmail.com>
2025-12-17 17:51:18 +0530

    Completed Implementing the Lottery State - Enum

--- Raffle/src/Raffle.sol
+++ Raffle/src/Raffle.sol
@@ -15,7 +15,15 @@ contract Raffle is VRFConsumerBaseV2Plus {
     /* ERRORS */
     error Raffle__SendMoreToEnterRaffle();
     error Raffle__TransferFailed();
+    error Raffle_RaffleNotOpen();
 
+    // Type Declarations
+    enum RaffleState {
+        OPEN,             // 0  
+        CALCULATING       // 1 
+    }
+
+    // State Variable
     uint16 private constant REQUEST_CONFIRMATIONS = 3;
     uint32 private constant NUM_WORDS = 1;
     uint256 private immutable i_enteranceFee;
@@ -27,6 +35,9 @@ contract Raffle is VRFConsumerBaseV2Plus {
     address payable[] private s_players;
     uint256 private s_lastTimeStamp;
     address private s_recentWinner;
+    RaffleState private s_raffleState;
+
+
 
     // events
     event RaffleEntered(address indexed player);
@@ -41,16 +52,21 @@ contract Raffle is VRFConsumerBaseV2Plus {
     ) VRFConsumerBaseV2Plus(vrfCoordinator) {
         i_enteranceFee = enteranceFee;
         i_interval = interval;
-        s_lastTimeStamp = block.timestamp;
         i_keyHash = gasLane;
         i_subscriptionId = subscriptionId;
         i_callbackGasLimit = callbackGasLimit;
+
+        s_lastTimeStamp = block.timestamp;
+        s_raffleState = RaffleState.OPEN; // same as RaffleState(0)
     }
 
     function enterRaffle() external payable {
         // require(msg.value >= i_enteranceFee, "Not Enough ETH Send");
         if (msg.value < i_enteranceFee) {
             revert Raffle__SendMoreToEnterRaffle();
+        }
+        if (s_raffleState != RaffleState.OPEN){
+            revert Raffle_RaffleNotOpen(); 
         }
         s_players.push(payable(msg.sender));
 
@@ -62,6 +78,9 @@ contract Raffle is VRFConsumerBaseV2Plus {
         if ((block.timestamp - s_lastTimeStamp) < i_interval) {
             revert();
         }
+
+        s_raffleState = RaffleState.CALCULATING;
+
 
         VRFV2PlusClient.RandomWordsRequest memory request = VRFV2PlusClient.RandomWordsRequest(
             {
@@ -90,6 +109,8 @@ contract Raffle is VRFConsumerBaseV2Plus {
         uint256 indexOfWinner = randomWords[0] % s_players.length;
         address payable recentWinner = s_players[indexOfWinner];
         s_recentWinner = recentWinner;
+        s_raffleState = RaffleState.OPEN;
+
         (bool success,) = recentWinner.call{value: address(this).balance}("");
         if(!success) {
             revert Raffle__TransferFailed();
